6e49e5b1aa13377f34f88955f3645652cb682810
odewale emmanuel <odebzemmygee@gmail.com>
2025-09-29 08:04:51 +0100

    task completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { NavBar } from "@/components/nav-bar";
+import { HeroSection } from "@/components/hero";
+import { GenerateSection } from "@/components/generate";
+import { GallerySection } from "@/components/gallery";
+import { StickyBanner } from "@/components/sticky-banner";
 
 export default function Home() {
   return (
-    <div className="min-w-screen backdrop-blur-lg">
+    <div className="">
       <NavBar />
+      <HeroSection />
+      <GenerateSection />
+      <GallerySection />
+      <StickyBanner />
     </div>
   );
 }
