de918c214519c04e23a98d7f21bc4da32f5858e6
odewale emmanuel <odebzemmygee@gmail.com>
2025-09-30 21:16:02 +0100

    open btn dark mode restyled

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -6,10 +6,10 @@ export function GallerySection(): JSX.Element {
   return (
     <section className="flex flex-col my-6 gap-1 px-4 sm:px-6 mb-4">
       <header className="flex items-center justify-between gap-2">
-        <h3 className="text-sm">Gallery</h3>
+        <h3 className="text-sm font-semibold sm:font-normal">Gallery</h3>
 
         <div className="inline-flex gap-2">
-          <span className="inline-flex gap-1.5 text-[.55em] relative bg-[#F6F6F6] dark:text-black py-2 px-2.5 rounded-xl">
+          <span className="inline-flex gap-1.5 text-[.55em] relative bg-[#F6F6F6] dark:border dark:bg-white/10 dark:text-white py-2 px-2.5 rounded-xl">
             <Link
               href={"/legal"}
               className="absolute w-full h-full top-0 left-0"
@@ -18,7 +18,7 @@ export function GallerySection(): JSX.Element {
             Legal
           </span>
 
-          <span className="inline-flex gap-1.5 text-[.55em] relative bg-[#F6F6F6] dark:text-black py-2 px-2.5 rounded-xl">
+          <span className="inline-flex gap-1.5 text-[.55em] relative bg-[#F6F6F6] dark:border dark:bg-white/10 dark:text-white py-2 px-2.5 rounded-xl">
             <Link
               href={"/pricing"}
               className="absolute w-full h-full top-0 left-0"
